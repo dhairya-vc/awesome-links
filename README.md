9a8e5eed08685e93b66f65b110e970d455290a92
Dhairya <dhairya.rathod@viitor.cloud>
2022-12-01 13:53:32 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Awesome Links
+
+[Fullstack app with TypeScript, Next.js, Prisma & GraphQL](https://prisma.io/blog/fullstack-nextjs-graphql-prisma-oklidw1rhw)
+
+<br/>
+
+# Add prisma to project
+
+Install Prisma's CLI as a development dependency:
+
+`npm install prisma -D`
+
+Create a basic Prisma setup by running:
+
+`npx prisma init`
---
+++ data/links.ts
@@ -0,0 +1,38 @@
+export const links = [
+  {
+    category: "Open Source",
+    description: "Fullstack React framework",
+    id: "8a9020b2-363b-4a4f-ad26-d6d55b51bqes",
+    imageUrl:
+      "https://res.cloudinary.com/practicaldev/image/fetch/s--80xZFtPO--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://nextjsnotes.com/content/images/2020/10/nextjs-conf-highlights-hero.jpg",
+    title: "Next.js",
+    url: "https://nextjs.org",
+  },
+  {
+    category: "Open Source",
+    description: "Next Generation ORM for TypeScript and JavaScript",
+    id: "2a3121b2-363b-4a4f-ad26-d6c35b41bade",
+    imageUrl:
+      "https://res.cloudinary.com/practicaldev/image/fetch/s--iDkWTEBC--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/i/3bzg30pauwbl828gcl84.png",
+
+    title: "Prisma",
+    url: "https://prisma.io",
+  },
+  {
+    category: "Open Source",
+    description: "Utility-fist css framework",
+    id: "6a9122b2-363b-4a4f-ad26-d6c55b51baed",
+    imageUrl:
+      "https://res.cloudinary.com/practicaldev/image/fetch/s--tWu0arYj--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://thepracticaldev.s3.amazonaws.com/i/vt7favk0dtktirx8an9p.jpg",
+    title: "TailwindCSS",
+    url: "https://tailwindcss.com",
+  },
+  {
+    category: "Open Source",
+    description: "GraphQL implementation ",
+    id: "2ea8cfb0-44a3-4c07-bdc2-31ffa135ea78",
+    imageUrl: "https://www.apollographql.com/apollo-home.jpg",
+    title: "Apollo GraphQL",
+    url: "https://apollographql.com",
+  },
+];
